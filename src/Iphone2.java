c8a6d790c439cb2485cd19bdd289f0522b0d6dd1
saru928 <kana.softball5@gmail.com>
2024-08-09 21:25:12 +0900

    ensyu7

---
+++ src/Iphone2.java
@@ -0,0 +1,15 @@
+
+public class Iphone2 {
+
+	public static void main(String[] args) {
+		SmartPhone2 iphone2 = new SmartPhone2();
+		iphone2.play();
+		iphone2.stop();
+		iphone2.next();
+		iphone2.back();
+		iphone2.call();
+		iphone2.mail();
+		iphone2.photo();
+		iphone2.internet();
+	}
+}
